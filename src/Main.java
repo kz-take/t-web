3171232e00133e7b69d994a0ec3d1d5a27f734d6
kz-take <k-take@xa3.so-net.ne.jp>
2019-09-05 23:36:26 +0900

    Main追加

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+package javaTest;
+
+public class Main {
+	public static void main(String... args) {
+		execPrinter(Main::printMessage, "foo");
+	}
+
+	static void execPrinter(Printer printer, String str) {
+		printer.print(str);
+	}
+
+	static void printMessage(String message) {
+		System.out.println(message);
+	}
+
+	interface Printer {
+		public void print(String msg);
+	}
+
+}
